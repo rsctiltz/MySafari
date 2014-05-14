edf61fd0d6a43c39653fbf6a1e8da634fd6e4b62
rsctiltz <rsctiltz@gmail.com>
2014-05-14 15:26:34 -0500

    Hackwich Day 3
    Buttons

--- MySafari/RTWebViewController.m
+++ MySafari/RTWebViewController.m
@@ -8,8 +8,10 @@
 
 #import "RTWebViewController.h"
 
-@interface RTWebViewController ()
+@interface RTWebViewController () <UIWebViewDelegate, UITextFieldDelegate>
 @property (strong, nonatomic) IBOutlet UIWebView *myWebView;
+@property (strong, nonatomic) IBOutlet UITextField *myURLTextField;
+@property (strong, nonatomic) IBOutlet UIButton *onBackButtonPressed;
 
 @end
 
@@ -19,6 +21,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)onBackButtonPressed:(UIButton *)sender
+{
+    [self.myWebView goBack];
+}
+- (IBAction)onForwardButtonPressed:(UIButton *)sender
+{
+    [self.myWebView goForward];
+}
+- (IBAction)onStopLoadingButtonPressed:(UIButton *)sender
+{
+    [self.myWebView stopLoading];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSURL *url = [NSURL URLWithString:textField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.myWebView loadRequest:request];
+
+    [textField resignFirstResponder];
+
+    return YES;
 }
 
 @end
